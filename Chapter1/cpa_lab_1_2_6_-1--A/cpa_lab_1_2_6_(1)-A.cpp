1430456070df272fc2ff0bed11c273003bc99ca9
1234bpv <33402738+1234bpv@users.noreply.github.com>
2017-12-17 08:31:33 +0200

    Create cpa_lab_1_2_6_(1)-A.cpp

---
+++ Chapter1/cpa_lab_1_2_6_-1--A/cpa_lab_1_2_6_(1)-A.cpp
@@ -0,0 +1,13 @@
+// cpa_lab_1_2_6_(1)-A.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	cout << "It's me, your first program.";
+	system("pause");
+	return 0;
+
+}
