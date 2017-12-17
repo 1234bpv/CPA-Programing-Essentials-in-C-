a214e17468705fddc16a5effae9116f7e6efa5d7
1234bpv <33402738+1234bpv@users.noreply.github.com>
2017-12-17 08:32:35 +0200

    Create cpa_lab_1_3_1_(1)-A.cpp

---
+++ Chapter1/cpa_lab_1_3_1_-1--A/cpa_lab_1_3_1_(1)-A.cpp
@@ -0,0 +1,19 @@
+// cpa_lab_1_3_1_(1)-A.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+int main()
+{
+	int result = 10800; // 3*60*60// This is a variable to hold the value of 3 hours in seconds
+	int threeMultipliedSixty = 3 * 60;// This is a variable to hold the value of 3 minutes in seconds
+	int fiveMultipliedSixty = 5 * 60;// This is a variable to hold the value of 5 minutes in seconds
+	float siii = 3.1415926; //This is the value of pi (я виправив)
+	std::cout << "result: " << result << std::endl;
+						   // here we should print the result value but a programmer didn't have time to write any code
+						   // but he/she likes writing long comments
+	system("pause");
+	return 0;
+}
