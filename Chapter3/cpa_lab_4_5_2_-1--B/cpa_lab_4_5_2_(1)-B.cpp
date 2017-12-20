1c554438ede65682261544f7162d9f07fc2be4f8
1234bpv <33402738+1234bpv@users.noreply.github.com>
2017-12-20 14:53:46 +0200

    Update cpa_lab_4_5_2_(1)-B.cpp

--- Chapter3/cpa_lab_4_5_2_-1--B/cpa_lab_4_5_2_(1)-B.cpp
+++ Chapter3/cpa_lab_4_5_2_-1--B/cpa_lab_4_5_2_(1)-B.cpp
@@ -7,27 +7,60 @@
 #include <cstdlib>
 using namespace std;
 
-int main(void) {
-	string sentence;
-	getline(std::cin, sentence);
-	// manipulate the sentence here
-	bool space = false;
+string checkIP(string input){
+	int result = 0;
+	int *ip, j = 0;
+	ip = new int[4];
 	string temp = "";
-	for (int i = 0; i < sentence.length(); i++){
-		if (sentence[i] == ' '){
-			if (!space){
-				temp += sentence[i];
-				space = true;
+	for (int i = 0; i < input.length(); i++){
+		if (input[i] == '.'){
+			if (temp.length() > 3){
+				if (result == 0)
+					result = 2;//Too many characters in a part == 2
+			}
+			try{
+				ip[j] = stoi(temp);
+				j++;
+				if (j > 4){
+					if (result == 0)
+						result = 3;//Too many parts == 3
+				}
+				temp = "";
+			}
+			catch(exception e){
+				if (result == 0)
+					result = 4;//Only digits and dots allowed == 4
 			}
 		}
 		else{
-			space = false;
-			temp += sentence[i];
+			temp += input[i];
 		}
 	}
-	sentence = temp;
+	switch (result)
+	{
+	case 2:
+		return "Too many characters in a part";
+		break;
+	case 3:
+		return "Too many parts";
+		break;
+	case 4:
+		return "Only digits and dots allowed";
+		break;
+	case 5:
+		return "Too big a value of a part";
+		break;
+	default:
+		return "Correct IP";//Correct IP == 1
+		break;
+	}
+}
 
-	cout << sentence << "\n";
+int main(void) {
+	string ip;
+	cout << "Enter IP: ";
+	cin >> ip;
+	cout << endl << checkIP(ip) << endl;
 
 	system("pause");
 	return 0;
